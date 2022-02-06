eb03387e2e2ab7d06c68f2655369fd21a5fa1dd7
Ravana <47528708+ravana69@users.noreply.github.com>
2022-02-06 19:49:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ particlesJS("particles-js", {
             },
         },
         color: {
-            value: "#b91d1d",
+            value: "#05c9ff",
         },
         shape: {
             type: "circle",
@@ -50,7 +50,7 @@ particlesJS("particles-js", {
         line_linked: {
             enable: true,
             distance: 150,
-            color: "#b91d1d",
+            color: "#05c9ff",
             opacity: 0.4,
             width: 1,
         },
